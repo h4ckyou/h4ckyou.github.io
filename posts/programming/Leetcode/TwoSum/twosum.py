74b122c32b7cf5738d942c8d5c972b7315177bc5
Mark Uchechukwu <127159644+h4ckyou@users.noreply.github.com>
2023-09-06 14:26:40 +0100

    Create twosum.py

---
+++ posts/programming/Leetcode/TwoSum/twosum.py
@@ -0,0 +1,19 @@
+def twoSum(nums, target):
+    hashtable = {}
+
+    for i, j in enumerate(nums):
+        complement = target - j
+
+        if complement in hashtable:
+            return [hashtable[complement], i]
+        
+        else:
+            hashtable[j] = i
+    
+    return None
+            
+nums = [2,7,11,15]
+target = 9
+
+result = twoSum(nums, target)
+print(result)
