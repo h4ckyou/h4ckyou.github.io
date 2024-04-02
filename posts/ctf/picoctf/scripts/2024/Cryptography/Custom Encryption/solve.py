4a80e8150551dce6eff0bf9801dce74e0912406b
Mark Uchechukwu <127159644+h4ckyou@users.noreply.github.com>
2024-04-02 13:14:50 +0100

    Update solve.py

--- posts/ctf/picoctf/scripts/2024/Cryptography/Custom Encryption/solve.py
+++ posts/ctf/picoctf/scripts/2024/Cryptography/Custom Encryption/solve.py
@@ -12,7 +12,7 @@ def xor_pwn(enc, key):
     pt = ""
     k_len = len(key)
 
-    for idx, val in enumerate(enc[::-1]):
+    for idx, val in enumerate(enc):
         k_chr = key[idx % k_len]
         d_chr = chr(ord(k_chr) ^ ord(val))
         pt += d_chr
@@ -32,4 +32,4 @@ shared_key = key
 
 cipher = [33588, 276168, 261240, 302292, 343344, 328416, 242580, 85836, 82104, 156744, 0, 309756, 78372, 18660, 253776, 0, 82104, 320952, 3732, 231384, 89568, 100764, 22392, 22392, 63444, 22392, 97032, 190332, 119424, 182868, 97032, 26124, 44784, 63444]
 semi_cipher = decrypt(cipher, shared_key)
-flag = xor_pwn(semi_cipher[::-1], "trudeau")
+flag = xor_pwn(semi_cipher, "trudeau")
