fd21cca3336fcf9b403e23d7e7f2c66606130a27
Mark Uchechukwu <127159644+h4ckyou@users.noreply.github.com>
2024-04-02 14:40:33 +0100

    Update solve.py

--- posts/ctf/picoctf/scripts/2024/Cryptography/RSA Oracle/solve.py
+++ posts/ctf/picoctf/scripts/2024/Cryptography/RSA Oracle/solve.py
@@ -1,48 +1,17 @@
 from pwn import *
-from Crypto.Util.number import long_to_bytes, bytes_to_long
-from math import gcd
-from warnings import filterwarnings
-import subprocess
 
 io = remote("titan.picoctf.net", 61923)
-context.log_level = 'info'
-filterwarnings("ignore")
-
-m1 = b'a'
-m2 = b'b'
 
 io.sendline("E")
-io.sendline(m1)
+io.sendline(p8(0x2))
 io.recvuntil("n) ")
-c1 = int(io.recvline().decode())
-
-io.sendline("E")
-io.sendline(m2)
-io.recvuntil("n) ")
-c2 = int(io.recvline().decode())
-
-e = 65537 
-
-k1n = (bytes_to_long(m1) ** e) - c1
-k2n = (bytes_to_long(m2) ** e) - c2
-
-n = gcd(k1n, k2n)
-
-assert c1 == pow(bytes_to_long(m1), e, n)
-
-print(f"n: {n}")
+ct1 = int(io.recvline().decode())
 
 with open("password.enc", "r") as fp:
-    ct = fp.read()
+    ct2 = fp.read()
 
-pad = n + int(ct)
-print(f"pad: {pad}")
+print(ct1)
+print(ct2)
 
-io.sendline("D")
-io.sendline(str(pad))
 
-io.recvuntil(": ")
-pt = bytes.fromhex(io.recvline().decode().split()[-1])
-print(f"AES Password: {pt}")
-
-io.close()
+io.interactive()
