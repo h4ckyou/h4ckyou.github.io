59892da594116b4de4fd7dc185c02b7b84bb5e5c
Mark Uchechukwu <127159644+h4ckyou@users.noreply.github.com>
2024-04-02 13:00:31 +0100

    Update solve.py

--- posts/ctf/picoctf/scripts/2024/Cryptography/Custom Encryption/solve.py
+++ posts/ctf/picoctf/scripts/2024/Cryptography/Custom Encryption/solve.py
@@ -1,66 +1,35 @@
-from random import randint
-import sys
-
-
 def generator(g, x, p):
     return pow(g, x) % p
 
+def decrypt(cipher, shared_key):
+    semi_cipher =  ""
+    for value in cipher:
+        semi_cipher += chr(value // (shared_key * 311))
 
-def encrypt(plaintext, key):
-    cipher = []
-    for char in plaintext:
-        cipher.append(((ord(char) * key*311)))
-    return cipher
+    return semi_cipher
 
+def xor_pwn(enc, key):
+    pt = ""
+    k_len = len(key)
 
-def is_prime(p):
-    v = 0
-    for i in range(2, p + 1):
-        if p % i == 0:
-            v = v + 1
-    if v > 1:
-        return False
-    else:
-        return True
-
-
-def dynamic_xor_encrypt(plaintext, text_key):
-    cipher_text = ""
-    key_length = len(text_key)
-    for i, char in enumerate(plaintext[::-1]):
-        key_char = text_key[i % key_length]
-        encrypted_char = chr(ord(char) ^ ord(key_char))
-        cipher_text += encrypted_char
-    return cipher_text
-
-
-def test(plain_text, text_key):
-    p = 97
-    g = 31
-    if not is_prime(p) and not is_prime(g):
-        print("Enter prime numbers")
-        return
-    a = randint(p-10, p)
-    b = randint(g-10, g)
-    print(f"a = {a}")
-    print(f"b = {b}")
-
-    u = generator(g, a, p)
-    v = generator(g, b, p)
-    key = generator(v, a, p)
-    b_key = generator(u, b, p)
-    shared_key = None
-    if key == b_key:
-        shared_key = key
-    else:
-        print("Invalid key")
-        return
+    for idx, val in enumerate(enc[::-1]):
+        k_chr = key[idx % k_len]
+        d_chr = chr(ord(k_chr) ^ ord(val))
+        pt += d_chr
     
-    semi_cipher = dynamic_xor_encrypt(plain_text, text_key)
-    cipher = encrypt(semi_cipher, shared_key)
-    print(f'cipher is: {cipher}')
+    print(pt[::-1])
 
+a, b = 89, 27
+p, g = 97, 31
 
-if __name__ == "__main__":
-    message = sys.argv[1]
-    test(message, "trudeau")
+u = generator(g, a, p)
+v = generator(g, b, p)
+key = generator(v, a, p)
+b_key = generator(u, b, p)
+
+assert key == b_key
+shared_key = key
+
+cipher = [33588, 276168, 261240, 302292, 343344, 328416, 242580, 85836, 82104, 156744, 0, 309756, 78372, 18660, 253776, 0, 82104, 320952, 3732, 231384, 89568, 100764, 22392, 22392, 63444, 22392, 97032, 190332, 119424, 182868, 97032, 26124, 44784, 63444]
+semi_cipher = decrypt(cipher, shared_key)
+flag = xor_pwn(semi_cipher[::-1], "trudeau")
