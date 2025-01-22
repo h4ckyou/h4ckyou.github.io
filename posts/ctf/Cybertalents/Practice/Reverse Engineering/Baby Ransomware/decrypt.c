2cc28b3b7c62293473bdfc298083e694b35d41c9
Mark Uchechukwu <127159644+h4ckyou@users.noreply.github.com>
2025-01-22 23:20:09 +0100

    Update decrypt.c

--- posts/ctf/Cybertalents/Practice/Reverse Engineering/Baby Ransomware/decrypt.c
+++ posts/ctf/Cybertalents/Practice/Reverse Engineering/Baby Ransomware/decrypt.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <time.h>
 
 struct Table {
     uint8_t array[256];
@@ -9,54 +10,94 @@ struct Table {
     uint32_t j;
 };
 
-struct Table table;
+struct Table *table;
 char *content;
 char *key = "Troll1337";
+// char key[16] = "Hell";
 
-void swap(uint8_t i, uint8_t j) {
-    uint8_t value = table.array[i];
-    table.array[i] = table.array[j];
-    table.array[j] = value;
+void swap(uint32_t i, uint32_t j) {
+    uint32_t value = table->array[i];
+    table->array[i] = table->array[j];
+    table->array[j] = value;
 }
 
 void init_table() {
+
+    table = malloc(sizeof(struct Table));
+    memset(table, 0, sizeof(struct Table));
     uint32_t size = strlen(key);
 
     for (size_t i = 0; i <= 255; i++) {
-        table.array[i] = i;
+        table->array[i] = i;
     }
 
-    table.j = 0;
+    table->j = 0;
 
-    for (table.i = 0; table.i <= 255; table.i++) {
-        table.j = (table.array[table.i] + table.j + key[table.i % size]) % 256;
-        swap(table.i, table.j);
-        // printf("val: %d\n", table.j);
+    for (table->i = 0; table->i <= 255; table->i++) {
+        table->j = (table->array[table->i] + table->j + key[table->i % size]) % 256;
+        swap(table->i, table->j);
+        // printf("val: %d\n", table->j);
     }
     
-    table.j = 0;
-    table.i = 0;
+    table->j = 0;
+    table->i = 0;
 
 }
-
 
 uint8_t get_byte(){
-    table.i = (table.i + 1) % 256;
-    table.j = (table.j + table.array[table.i]) % 256;
-    swap(table.i, table.j);
-    return table.array[(table.array[table.i] + table.array[table.j])];
+    table->i = (table->i + 1) % 256;
+    table->j = (table->j + table->array[table->i]) % 256;
+    swap(table->i, table->j);
+    return table->array[(uint8_t)(table->array[table->i] + table->array[table->j])];
 }
 
-
 void decrypt(long size) {
-    init_table();
-
-    for (size_t i = 0; i < size; i++){
-        uint8_t key = get_byte();
-        printf("%c", content[i] ^ key);
-    }
     
+        init_table();
 
+        for (size_t i = 0; i < size; i++) {
+            uint8_t key = get_byte();
+            printf("%c", content[i] ^ key);
+        }
+
+        free(table);
+}
+
+void brute(long size) {
+    char digit_str[2] = {0};
+    int value = 0;
+    uint32_t start = 0x67916000;
+    uint32_t end = start + 0x1000;
+
+    for (uint32_t seed = start; seed <= end; seed++) {
+        srand(seed);
+
+        for (int i = 0; i <= 4; i++) {
+            value = rand() % 10;
+            snprintf(digit_str, 2, "%d", value);
+            strncat(key, digit_str, 1);
+        }
+
+        init_table();
+
+        // for (size_t i = 0; i <= size; i++){
+        //     uint8_t key1 = get_byte();
+        //     if (key == 0xa4)
+        // }
+
+        uint8_t key1 = get_byte();
+        uint8_t key2 = get_byte();
+        uint8_t key3 = get_byte();
+
+        putchar(0xa);
+        memset(key, 0, 16);
+        strncpy(key, "Hell", 4);
+
+        free(table);
+        table = NULL;
+    }
+
+    
 }
 
 int main(int argc, char *argv[]) {
@@ -88,4 +129,3 @@ int main(int argc, char *argv[]) {
     decrypt(size);
 
 }
-
